0600d98ad69aa8ef4bb4574402740898171f3eb5
GitHub-Harrison <harrison-johnson@outlook.com>
2021-11-19 14:04:44 +0000

    Add basic code layout for Players

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
-print("Hello World!")
+import math
+import random
+
+# Code for the players Human/Computer
+class Player:
+    def __init__(self, letter):
+        # letter is X or O
+        self.letter = letter
+
+    # for players to get their next move
+    def get_move(self, game):
+        pass # Continue this later
+
+class ComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
