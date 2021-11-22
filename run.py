6a8e1fa8245f22328e97a0af2f212e43e2164503
GitHub-Harrison <harrison-johnson@outlook.com>
2021-11-22 01:59:39 +0000

    Add line row separations

--- run.py
+++ run.py
@@ -8,7 +8,9 @@ gameRunning = True
 # print the game board
 def print_board(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------")
     print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------")
     print(board[6] + " | " + board[7] + " | " + board[8])
 
 # take player input
