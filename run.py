af6c2658005b83db96025113af7b9116fabf8406
GitHub-Harrison <harrison-johnson@outlook.com>
2021-11-22 03:02:48 +0000

    Add code to check for horizontal win

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ board = ["-", "-", "-",
         "-", "-", "-"]
 current_player = "X"
 current_winner = None
-gameRunning = True
+game_running = True
 
 # print the game board
 def print_board(board):
@@ -23,7 +23,22 @@ def playerInput(board):
         print("A player is already in that spot.")
 
 # check for win or tie
+def check_hor(board):
+    global current_winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
 
 # switch the player
 
 # check for win or tie again
+
+while game_running:
+    print_board(board)
+    playerInput(board)
