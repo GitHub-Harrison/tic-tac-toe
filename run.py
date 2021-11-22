1015d26de1ec051a85b530f3a3467e5e5663590f
GitHub-Harrison <harrison-johnson@outlook.com>
2021-11-22 01:57:50 +0000

    Add code to print out game board

--- run.py
+++ run.py
@@ -6,6 +6,10 @@ currentWinner = None
 gameRunning = True
 
 # print the game board
+def print_board(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
 
 # take player input
 
