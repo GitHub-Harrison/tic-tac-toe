9f8c0211ba66e8afbce1879dd85e353c9959f8f2
GitHub-Harrison <harrison-johnson@outlook.com>
2021-11-22 03:05:26 +0000

    Add code to check for vertical win

--- run.py
+++ run.py
@@ -26,13 +26,25 @@ def playerInput(board):
 def check_hor(board):
     global current_winner
     if board[0] == board[1] == board[2] and board[1] != "-":
-        winner = board[0]
+        current_winner = board[0]
         return True
     elif board[3] == board[4] == board[5] and board[3] != "-":
-        winner = board[3]
+        current_winner = board[3]
         return True
     elif board[6] == board[7] == board[8] and board[6] != "-":
-        winner = board[6]
+        current_winner = board[6]
+        return True
+
+def check_vert(board):
+    global current_winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        current_winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        current_winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        current_winner = board[2]
         return True
 
 # switch the player
